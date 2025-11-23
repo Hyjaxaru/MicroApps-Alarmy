d8ca8e86770c1faa6e861f70ecdead8b4be4da85
Hyjaxaru <noah.albrock@gmail.com>
2025-11-23 15:31:52 +0000

    Initial Commit

---
+++ RioAlarm/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RioAlarm
+//
+//  Created by Noah Albrock on 23/11/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RioAlarm/RioAlarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  RioAlarmApp.swift
+//  RioAlarm
+//
+//  Created by Noah Albrock on 23/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct RioAlarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
